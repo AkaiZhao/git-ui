2cd60f3bab52b5a7e4e1711252c999890998ed06
Akai <keivn910171@gmail.com>
2020-12-29 14:47:42 +0800

    remove template

--- template/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './style.css'
-
-const Library = ({ name }) => {
-  return <div>Hello, world!</div>
-}
-
-export default Library
--- template/src/index.test.jsx
+++
@@ -1,6 +0,0 @@
-import Component from './index'
-
-test('test library', () => {
-  const wrapper = mount(<Component />)
-  wrapper.
-})
